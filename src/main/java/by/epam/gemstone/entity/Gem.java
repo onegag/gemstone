2673c1d1cd383f5cc9a7e8a791676aa013d3833a
Hanna Tamkovich <annyat96@gmail.com>
2019-12-24 16:14:46 +0300

    gem and gems classes

--- src/main/java/by/epam/gemstone/entity/Gem.java
+++ src/main/java/by/epam/gemstone/entity/Gem.java
@@ -4,13 +4,33 @@ package by.epam.gemstone.entity;
 import by.epam.gemstone.entity.type.GemColor;
 import by.epam.gemstone.entity.type.Rarity;
 
+import javax.xml.bind.annotation.*;
 
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "gem", namespace = "http://www.gemstone.epam.by/gems")
+@XmlType(name = "Gem", propOrder = {
+        "color",
+        "hardnessPercent",
+        "facetsQuantity",
+        "weightCarat",
+        "rarity"
+})
+@XmlSeeAlso({
+        NaturalGem.class,
+        SyntheticGem.class
+})
 public abstract class Gem {
-
-    GemColor color;
+    private String name;
+    @XmlElement(name = "color", required = true)
+    private GemColor color;
+    @XmlElement(name = "hardness_percent", required = true)
     private int hardnessPercent;
-    int fasetsQuantity;
-    double weightCarat;
-    Rarity rarity;
+    @XmlElement(name = "facets_quantity", defaultValue = "0")
+    private int facetsQuantity;
+    @XmlElement(name = "weight_carat", required = true)
+    private double weightCarat;
+    @XmlElement(name = "rarity", required = true)
+    private Rarity rarity;
+
 
 }
--- src/main/java/by/epam/gemstone/entity/Gems.java
+++ src/main/java/by/epam/gemstone/entity/Gems.java
@@ -1,8 +1,31 @@
 package by.epam.gemstone.entity;
 
-import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
+
+@XmlAccessorType(XmlAccessType.FIELD)
+
+@XmlRootElement (name = "gems")
 public class Gems {
-    //@XmlAccessorType
+    @XmlElementRefs(value = {@XmlElementRef(name = "gem"),
 
+            @XmlElementRef(name = "natural_gem"),
+            @XmlElementRef(name = "synthetic_gem"),
+            @XmlElementRef(name = "organic_gem"),
+            @XmlElementRef(name = "presious_gem"),
+            @XmlElementRef(name = "semipresious_gem")})
+    private List<Gem> gems =new ArrayList<Gem>();
+
+    public Gems() {
+    }
+
+    public List<Gem> getGems() {
+        return gems;
+    }
+
+    public void setGems(List<Gem> gems) {
+        this.gems = gems;
+    }
 }
